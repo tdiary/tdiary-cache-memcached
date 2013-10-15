5098267adedd75d5c9357bcc8203a43664c7afa6
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-10-16 08:34:11 +0900

    bundle gem tdiary-cache-memcached -t

---
+++ lib/tdiary/cache/memcached.rb
@@ -0,0 +1,9 @@
+require "tdiary/cache/memcached/version"
+
+module Tdiary
+  module Cache
+    module Memcached
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/tdiary/cache/memcached/version.rb
@@ -0,0 +1,7 @@
+module Tdiary
+  module Cache
+    module Memcached
+      VERSION = "0.0.1"
+    end
+  end
+end
